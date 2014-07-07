5c9b0a3e50224b034dfc8ea4b55534486c667785
MentalDude <apostol_dadachev@yahoo.com>
2014-07-07 23:44:50 +0200

    Added Debug glew library to fix all the annoying linker warnings. Added physfs.h file system library which also handles loading files from archives.

--- Lux/Lux/Include/LuxPCH.h
+++ Lux/Lux/Include/LuxPCH.h
@@ -20,8 +20,12 @@
 #include <GLFW/glfw3.h>
 
 // Math library
+#define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
+
+// File system library
+#include <physfs.h>
 
 // Engine specific include
 #include "LuxLog.h"
