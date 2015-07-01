b96e91ad1504d31008e4fbafa1f45f7ac53e70e0
MentalDude <apostol_dadachev@yahoo.com>
2015-07-01 23:58:29 +0200

    Cleaned up SceneManager.h and moved all implementations of non-template functions to the .cpp file.

--- Lux/Lux/Include/LuxSceneManager.h
+++ Lux/Lux/Include/LuxSceneManager.h
@@ -224,51 +224,9 @@ namespace Lux
 
 		private:
 
-			void AddDependenciesRecursive(const Key& a_CurrCompKey, const Key& a_CheckKey, unsigned int a_Depth)
-			{
-				if (a_Depth > LUX_COMPONENT_DEPENDENCIES_MAX_RECURSION)
-				{
-					Utility::ThrowError("Component Dependency Max Recursion Reached.");
-				}
+			void AddDependenciesRecursive(const Key& a_CurrCompKey, const Key& a_CheckKey, unsigned int a_Depth);
 
-				bool keyExists = m_ComponentDependencyMap.count(a_CheckKey) > 0;
-				if (!keyExists)
-					return;
-
-				ComponentDependencyMap::iterator iter1 = m_ComponentDependencyMap.lower_bound(a_CheckKey);
-				ComponentDependencyMap::iterator iter2 = m_ComponentDependencyMap.upper_bound(a_CheckKey);
-
-				while (iter1 != iter2)
-				{
-					// Make sure we don't add the same dependency twice
-					if (ComponentDependencyExists(a_CurrCompKey, iter1->second))
-					{
-						++iter1;
-						continue;
-					}
-
-					m_ComponentDependencyMap.insert(std::make_pair(a_CurrCompKey, iter1->second));
-
-					AddDependenciesRecursive(a_CurrCompKey, iter1->second, a_Depth++);
-
-					++iter1;
-				}
-			}
-
-			bool ComponentDependencyExists(const Key& a_Key, const Key& a_DepKey)
-			{
-				ComponentDependencyMap::iterator it1 = m_ComponentDependencyMap.lower_bound(a_Key);
-				ComponentDependencyMap::iterator it2 = m_ComponentDependencyMap.upper_bound(a_Key);
-
-				while (it1 != it2)
-				{
-					if (it1->second == a_DepKey)
-						return true; // Already added, just return
-
-					++it1;
-				}
-				return false;
-			}
+			bool ComponentDependencyExists(const Key& a_Key, const Key& a_DepKey);
 
 			template<class ComponentType>
 			void AttachDependentComponent(ObjectHandle<Entity>& a_Ent)
@@ -324,10 +282,7 @@ namespace Lux
 
 			void ProcessSystems(const float a_DeltaTime);
 
-			ComponentLayout& GetComponentLayout(ObjectHandle<Entity>& a_Entity)
-			{
-				return m_EntityComponentMap.at(&a_Entity);
-			}
+			ComponentLayout& GetComponentLayout(ObjectHandle<Entity>& a_Entity);
 
 			ComponentFactory* m_ComponentFactory;
 			EntityFactory* m_EntityFactory;
@@ -355,50 +310,17 @@ namespace Lux
 
 		struct ComponentLayout
 		{
-			ComponentLayout(const unsigned int a_NumComponents, ObjectHandle<Entity>& a_Owner, SceneManager* a_Manager) :
-				m_NumComponents(a_NumComponents), m_Owner(a_Owner), m_SceneManager(a_Manager)
-			{
-				m_Components = new ComponentEntry[m_NumComponents];
-			}
+			ComponentLayout(const unsigned int a_NumComponents, ObjectHandle<Entity>& a_Owner, SceneManager* a_Manager);
 
-			ComponentLayout(const ComponentLayout& a_Layout) : m_Owner(a_Layout.m_Owner)
-			{
-				m_NumComponents = a_Layout.m_NumComponents;
-				m_SceneManager = a_Layout.m_SceneManager;
-				m_Components = new ComponentEntry[m_NumComponents];
-				for (unsigned int i = 0; i < m_NumComponents; i++)
-				{
-					m_Components[i].m_Data = a_Layout.m_Components->m_Data;
-				}
-			}
+			ComponentLayout(const ComponentLayout& a_Layout);
 
-			~ComponentLayout()
-			{
-				SceneManager::ComponentDelFuncMap::iterator it;
-
-				unsigned int ctr = 0;
-				for (it = m_SceneManager->m_DelFuncMap.begin(); it != m_SceneManager->m_DelFuncMap.end(); ++it)
-				{
-					if (m_Components[it->first].m_Data != nullptr)
-					{
-						it->second(m_Components[it->first].m_Data, m_Owner);
-					}
-				}
-
-				Utility::SafeArrayDelete(m_Components);
-			}
+			~ComponentLayout();
 
 			struct ComponentEntry
 			{
-				ComponentEntry() : m_Data(nullptr)
-				{
+				ComponentEntry();
 
-				}
-
-				~ComponentEntry()
-				{
-					m_Data = nullptr;
-				}
+				~ComponentEntry();
 
 				void* m_Data;
 			};
--- Lux/Lux/Source/LuxSceneManager.cpp
+++ Lux/Lux/Source/LuxSceneManager.cpp
@@ -80,3 +80,97 @@ void Lux::Core::SceneManager::ProcessSystems(const float a_DeltaTime)
 		systemPtr->ProcessUpdate(a_DeltaTime);
 	}
 }
+
+void Lux::Core::SceneManager::AddDependenciesRecursive(const Key& a_CurrCompKey, const Key& a_CheckKey, unsigned int a_Depth)
+{
+	if (a_Depth > LUX_COMPONENT_DEPENDENCIES_MAX_RECURSION)
+	{
+		Utility::ThrowError("Component Dependency Max Recursion Reached.");
+	}
+
+	bool keyExists = m_ComponentDependencyMap.count(a_CheckKey) > 0;
+	if (!keyExists)
+		return;
+
+	ComponentDependencyMap::iterator iter1 = m_ComponentDependencyMap.lower_bound(a_CheckKey);
+	ComponentDependencyMap::iterator iter2 = m_ComponentDependencyMap.upper_bound(a_CheckKey);
+
+	while (iter1 != iter2)
+	{
+		// Make sure we don't add the same dependency twice
+		if (ComponentDependencyExists(a_CurrCompKey, iter1->second))
+		{
+			++iter1;
+			continue;
+		}
+
+		m_ComponentDependencyMap.insert(std::make_pair(a_CurrCompKey, iter1->second));
+
+		AddDependenciesRecursive(a_CurrCompKey, iter1->second, a_Depth++);
+
+		++iter1;
+	}
+}
+
+bool Lux::Core::SceneManager::ComponentDependencyExists(const Key& a_Key, const Key& a_DepKey)
+{
+	ComponentDependencyMap::iterator it1 = m_ComponentDependencyMap.lower_bound(a_Key);
+	ComponentDependencyMap::iterator it2 = m_ComponentDependencyMap.upper_bound(a_Key);
+
+	while (it1 != it2)
+	{
+		if (it1->second == a_DepKey)
+			return true; // Already added, just return
+
+		++it1;
+	}
+	return false;
+}
+
+Lux::Core::ComponentLayout& Lux::Core::SceneManager::GetComponentLayout(ObjectHandle<Entity>& a_Entity)
+{
+	return m_EntityComponentMap.at(&a_Entity);
+}
+
+Lux::Core::ComponentLayout::ComponentEntry::~ComponentEntry()
+{
+	m_Data = nullptr;
+}
+
+Lux::Core::ComponentLayout::ComponentEntry::ComponentEntry() : m_Data(nullptr)
+{
+
+}
+
+Lux::Core::ComponentLayout::~ComponentLayout()
+{
+	SceneManager::ComponentDelFuncMap::iterator it;
+
+	unsigned int ctr = 0;
+	for (it = m_SceneManager->m_DelFuncMap.begin(); it != m_SceneManager->m_DelFuncMap.end(); ++it)
+	{
+		if (m_Components[it->first].m_Data != nullptr)
+		{
+			it->second(m_Components[it->first].m_Data, m_Owner);
+		}
+	}
+
+	Utility::SafeArrayDelete(m_Components);
+}
+
+Lux::Core::ComponentLayout::ComponentLayout(const ComponentLayout& a_Layout) : m_Owner(a_Layout.m_Owner)
+{
+	m_NumComponents = a_Layout.m_NumComponents;
+	m_SceneManager = a_Layout.m_SceneManager;
+	m_Components = new ComponentEntry[m_NumComponents];
+	for (unsigned int i = 0; i < m_NumComponents; i++)
+	{
+		m_Components[i].m_Data = a_Layout.m_Components->m_Data;
+	}
+}
+
+Lux::Core::ComponentLayout::ComponentLayout(const unsigned int a_NumComponents, ObjectHandle<Entity>& a_Owner, SceneManager* a_Manager) :
+m_NumComponents(a_NumComponents), m_Owner(a_Owner), m_SceneManager(a_Manager)
+{
+	m_Components = new ComponentEntry[m_NumComponents];
+}
